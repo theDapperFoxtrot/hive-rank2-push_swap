854c02efa1a35fb02945ecf9734ff585c59bcfce
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2024-10-23 17:01:07 +0300

    brainstorming

--- main_push_swap.c
+++ main_push_swap.c
@@ -19,17 +19,23 @@ int	main(int argc, char **argv)
 	t_stack *stack_a = (t_stack *)malloc(sizeof(t_stack));
     t_stack *stack_b = (t_stack *)malloc(sizeof(t_stack));
 	t_data	data;
+	char	***temp_argv;
+	int		i;
 
+	i = 0;
 	data.argc = argc;
 	malloc_check(stack_a, stack_b, &data);
 	// Check if there are no arguments
 	if ((argc == 1) || (argc == 2 && !argv[1][0]))
 		return (1);
-	// If there is only one argument, split it
-	if (argc == 2)
-		argc_2(stack_a, stack_b, argv, &data);
-	else
-		argc_above_2(stack_a, stack_b, argv, &data);
+	// Split the arguments
+	while (i < argc - 1)
+	{
+		temp_argv[i] = ft_split(argv[i + 1], ' ');
+		i++;
+	}
+	// then I need to join these, and free this abomination
+	set_default_values(stack_a, stack_b, argv, &data);
 	// Initialize the stacks with nodes
 	init_stack(stack_a, &data);
 	// I want to test printing out the stack of integers once they've been added
--- operations.c
+++ operations.c
@@ -4,10 +4,12 @@ void    sa(t_stack *stack_a)
 {
 	t_node  *temp;
 
+	// SWITCH VALUE
 	temp = stack_a->first_node->next; //second node stored in temp
 	stack_a->first_node->next = stack_a->first_node; //second spot gets first node
-	stack_a->first_node->current_position = stack_a->first_node->current_position - 1;
 	stack_a->first_node = temp; //put second node into first spot
+	// SWITCH POSITION
+	stack_a->first_node->current_position = stack_a->first_node->current_position - 1;
 	stack_a->first_node->current_position = stack_a->first_node->current_position + 1;
 // I NEED TO DRAW THIS OUT. MANY THINGS CHANGE IN THE STRUCT. POSSIBLE ABSTRACTION INTO HELPER FUNCTIONS NEEDED
 }
