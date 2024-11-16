f60f59ddc3ad2ea0b6425806f9dcc8f8fbb7e400
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2024-11-16 19:23:35 +0200

    working bad sort

--- make_moves.c
+++ make_moves.c
@@ -12,7 +12,7 @@ static void get_stuff_in_b(t_stack *stack_a, t_stack *stack_b, t_data *data)
             pb(stack_a, stack_b, data);
             target_node++;
         }
-        else if (stack_a->first_node->next->final_position == target_node)
+        else if (stack_a->first_node->next != NULL && stack_a->first_node->next->final_position == target_node)
         {
             sa(stack_a, 0, data);
             pb(stack_a, stack_b, data);
@@ -20,12 +20,12 @@ static void get_stuff_in_b(t_stack *stack_a, t_stack *stack_b, t_data *data)
         }
         else if (stack_a->last_node->final_position == target_node)
         {
-            ra(stack_a, 0, data);
+            rra(stack_a, 0, data);
             pb(stack_a, stack_b, data);
             target_node++;
         }
         else
-            rotate_a(stack_a, 0, data);
+            ra(stack_a, 0, data);
     }
 }
 
